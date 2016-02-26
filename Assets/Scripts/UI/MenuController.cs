295b472ef44bad0f9e02c052799ae8feeed33167
= <anton@lyxit.se>
2016-02-26 22:12:17 +0100

    Add simple menu

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClickContinue() {
+		Application.LoadLevel("Level_0");
+	}
+
+	public void OnClickNewGame() {
+		Debug.Log("NewGame");
+	}
+
+	public void OnClickSettings() {
+		Debug.Log("Settings");
+	}
+
+	public void OnClickQuit() {
+		Debug.Log("Quit");
+	}
+}
