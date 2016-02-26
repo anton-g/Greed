1703e7462f32cf6b2624a7bca2dd9ed6035c50d8
= <anton@lyxit.se>
2016-02-26 19:30:28 +0100

    Improve beam disable/enable look

--- Assets/Scripts/Interactibles/BeamController.cs
+++ Assets/Scripts/Interactibles/BeamController.cs
@@ -1,8 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BeamController : MonoBehaviour {
+	public GameObject[] beams;
+	public ParticleSystem[] emitters;
+
+	Dictionary<ParticleSystem, float> emissionRates;
+
+	void Start() {
+		emissionRates = new Dictionary<ParticleSystem, float>();
+	}
+
 	public void Toggle() {
-		gameObject.SetActive(!gameObject.activeSelf);
+		//gameObject.SetActive(!gameObject.activeSelf);
+		foreach (var beam in beams) {
+			beam.SetActive(!beam.activeSelf);
+		}
+
+		foreach (var emitter in emitters) {
+			if (!emissionRates.ContainsKey(emitter)) {
+				emissionRates.Add(emitter, emitter.emissionRate);
+			}
+
+			float oldEmissionRate;
+			emissionRates.TryGetValue(emitter, out oldEmissionRate);
+			emitter.emissionRate = emitter.emissionRate == 0.0f ? oldEmissionRate : 0.0f;
+		}
 	}
 }
