96805c95200b62900c473f5809d0838f8ff46d50
= <anton@lyxit.se>
2016-04-06 01:52:59 +0200

    Add script to make objects bob

---
+++ Assets/Scripts/Common/Bobbing.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bobbing : MonoBehaviour {
+
+    public float bobDistance = 0.4f;
+	
+    Vector3 currentTarget;
+    Vector3 origPos;
+    
+    Vector3 velocity;
+    float smoothTime = 0.25f;
+    float speed = 0.5f;
+    
+    void Start() {
+        origPos = transform.position;
+        currentTarget = new Vector3(transform.position.x, transform.position.y + bobDistance, transform.position.z);
+    }
+    
+	// Update is called once per frame
+	void Update () {
+	    transform.position = Vector3.SmoothDamp(transform.position, currentTarget, ref velocity, smoothTime, speed);
+        
+        if (transform.position.y > currentTarget.y - (bobDistance / 10) && transform.position.y < currentTarget.y + (bobDistance / 10)) {
+            Debug.Log(currentTarget.y);
+            if (transform.position.y > origPos.y) {
+                currentTarget = new Vector3(origPos.x, origPos.y - bobDistance, origPos.z);
+            } else {
+                currentTarget = new Vector3(origPos.x, origPos.y + bobDistance, origPos.z);
+            }
+        }
+	}
+}
