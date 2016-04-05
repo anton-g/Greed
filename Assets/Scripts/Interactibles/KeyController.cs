fb9d3dc4548e5cca5bf21356a7a151098dc7feab
= <anton@lyxit.se>
2016-04-06 01:08:54 +0200

    Add key controller

---
+++ Assets/Scripts/Interactibles/KeyController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyController : MonoBehaviour {
+
+    [Header("Key settings")]
+    public DoorController targetDoor;
+    
+    public void UnlockDoor() {
+        targetDoor.Open();
+        
+        gameObject.SetActive(false);
+    }
+}
