8e2f5b0ea6d028527b407600e9bd5ebd826b3845
= <anton@lyxit.se>
2016-04-05 17:39:28 +0200

    Restructure Rotator script
    Move Editor specific code to separate script

---
+++ Assets/Scripts/Common/Rotator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+    [Header("Test")]
+    public RotateBehaviour behaviour = RotateBehaviour.RotateAroundSelf;
+    public RotateDirection direction = RotateDirection.Clockwise;
+    public float speed = 20f;
+    
+    public Transform rotateTarget;
+    public Vector3 rotatePoint;
+
+	void Update () {
+        switch (behaviour)
+        {
+            case RotateBehaviour.RotateAroundSelf:
+            transform.Rotate(Vector3.forward * (int)direction * speed * Time.deltaTime);
+            break;
+            case RotateBehaviour.RotateAroundPoint:
+            transform.RotateAround(transform.TransformPoint(rotatePoint), Vector3.forward * (int)direction, speed * Time.deltaTime);
+            break;
+            case RotateBehaviour.RotateAroundObject:
+            if (rotateTarget != null) {
+                transform.RotateAround(rotateTarget.position, Vector3.forward * (int)direction, speed * Time.deltaTime);
+            } else {
+                Debug.LogError("Need to set object to rotate around.");
+            }
+            break;
+        }
+	}
+    
+    void OnDrawGizmos() {
+        if (behaviour == RotateBehaviour.RotateAroundPoint) {
+			Gizmos.color = Color.red;
+			float size = .3f;
+
+            Vector3 globalPoint = (Application.isPlaying) ? transform.TransformPoint(rotatePoint) : rotatePoint + transform.position;
+            Gizmos.DrawLine(globalPoint - Vector3.up * size, globalPoint + Vector3.up * size);
+            Gizmos.DrawLine(globalPoint - Vector3.left * size, globalPoint + Vector3.left * size);
+		}
+    }
+}
+public enum RotateDirection {
+    Clockwise = -1,
+    CounterClockwise = 1
+}
+
+public enum RotateBehaviour {
+    RotateAroundSelf,
+    RotateAroundPoint,
+    RotateAroundObject
+}
---
+++ Assets/Scripts/Editor/RotatorEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Rotator))]
+[CanEditMultipleObjects]
+public class RotatorEditor : Editor {
+    public override void OnInspectorGUI() {        
+        Rotator rotator = target as Rotator;
+        
+        rotator.behaviour = (RotateBehaviour)EditorGUILayout.EnumPopup("Behaviour:", rotator.behaviour);
+        rotator.direction = (RotateDirection)EditorGUILayout.EnumPopup("Direction:", rotator.direction);
+        rotator.speed = EditorGUILayout.FloatField("Speed:", rotator.speed);
+        
+        EditorGUILayout.Separator();
+        
+        switch (rotator.behaviour)
+        {
+            case RotateBehaviour.RotateAroundSelf:
+            break;
+            case RotateBehaviour.RotateAroundPoint:
+                rotator.rotatePoint = EditorGUILayout.Vector3Field("Rotate Point:", rotator.rotatePoint);
+            break;
+            case RotateBehaviour.RotateAroundObject:
+                rotator.rotateTarget = (Transform)EditorGUILayout.ObjectField("Target", rotator.rotateTarget, typeof(Transform), true);
+            break;
+        }
+        
+        if (GUI.changed) EditorUtility.SetDirty (target);
+    }
+}
