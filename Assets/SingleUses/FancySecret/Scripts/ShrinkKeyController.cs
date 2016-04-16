fbba6f81ea569e9cabcf2025708bc29c75ea0ed8
= <anton@lyxit.se>
2016-04-16 11:28:30 +0200

    Add shrink key controller

---
+++ Assets/SingleUses/FancySecret/Scripts/ShrinkKeyController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShrinkKeyController : KeyController {
+
+    public Shrink[] targets;
+    public float delay = 0.1f;
+	// Use this for initialization
+	public override void Collect() {
+        StartCoroutine(shrinkTargets());
+    }
+    
+    IEnumerator shrinkTargets() {
+        for (int i = 0; i < targets.Length; i++)
+        {
+            targets[i].isShrinking = true;
+            yield return new WaitForSeconds(delay);
+        }
+        
+        gameObject.SetActive(false);
+    }
+}
