58ca5500422f171a3af4320f49c28b662f144ca2
= <anton@lyxit.se>
2016-04-23 10:44:29 +0200

    Add AudioManager singleton

---
+++ Assets/Scripts/Game Handling/AudioManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+    public static AudioManager Instance = null;
+
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else if (Instance != this) {
+            Destroy(gameObject);
+        }    
+        DontDestroyOnLoad(gameObject);
+    }
+}
